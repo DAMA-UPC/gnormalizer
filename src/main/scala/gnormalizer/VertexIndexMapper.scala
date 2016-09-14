cb59f5cf172983d0d9d9b697c1b813db30dcc110
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2016-09-14 16:31:07 +0200

    Rename 'VertexIndexGenerator' to 'VertexIndexMapper'

--- src/main/scala/gnormalizer/VertexIndexMapper.scala
+++ src/main/scala/gnormalizer/VertexIndexMapper.scala
@@ -1,25 +1,26 @@
 package gnormalizer
 
-import cats.Id
 import gnormalizer.Vertex.Vertex
 
 import scala.collection.{immutable, mutable}
 
 /**
-  * Trait containing the method used for storing/generating the vertexes IDs.
+  * Trait containing the method used for retrieving the vertexes IDs. If a Vertex
+  * ID was not already generated, it generates, stores and retrieve it instead.
   */
-class VertexIndexGenerator {
+class VertexIndexMapper {
 
-  private[this] val vertexMappingCache : mutable.Map[String, Vertex] = {
+  private[this] val vertexMappingCache: mutable.Map[String, Vertex] = {
     mutable.HashMap.empty[String, Vertex]
   }
 
   /**
     * Returns the cache vertex Index if available. Generates a new one and returns it otherwise.
+    *
     * @param vertex from the index that will be retrieved.
-    * @return [[Long]] with the resulting index. (Starting from 0).
+    * @return [[Vertex]] with the resulting index. (Starting from 0).
     */
-  def vertexIndex(vertex : String) : Vertex = {
+  def vertexIndex(vertex: String): Vertex = {
     vertexMappingCache.get(vertex) match {
       case Some(index) => index
       case _ =>
@@ -32,12 +33,7 @@ class VertexIndexGenerator {
   }
 
   /**
-    * Obtains the mapping from all the indexed Vertex [[Id]].
+    * Obtains the mapping from all the indexed [[Vertex]] as an [[immutable.Map[[String, Vertex]]]]
     */
-  def vertexMapping : immutable.Map[String, Vertex] = vertexMappingCache.toMap
-
-  /**
-    * @return the number of indexed Vertexes.
-    */
-  def countIndexedVertex : Int = vertexMappingCache.size
+  def vertexMapping: immutable.Map[String, Vertex] = vertexMappingCache.toMap
 }
