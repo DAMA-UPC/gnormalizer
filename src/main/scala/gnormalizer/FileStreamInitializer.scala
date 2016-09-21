11f8e5b6b86b84c537b550951d076c69488150d2
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2016-09-21 16:55:43 +0200

    Implement 'FileStreamInitializer'
    - 'FileStreamInitializer' is an object containing a method used for
       initializing a 'FileStream' from an input file

---
+++ src/main/scala/gnormalizer/FileStreamInitializer.scala
@@ -0,0 +1,29 @@
+package gnormalizer
+
+import java.nio.file.Paths
+
+import fs2.{Task, io, text, Stream => FileStream}
+
+/**
+  * Object containing a set of methods used for initializing file [[FileStream]].
+  */
+object FileStreamInitializer {
+
+  @inline val defaultChunkSize: Int = 4096
+
+  /**
+    * Initializes a [[FileStream]] of an input file.
+    *
+    * @param path      where the [[FileStream]] will start.
+    * @param chunkSize from the [[FileStream]].
+    * @return a [[FileStream]] that will be return the graph contents as UTF-8 [[String]]'s.
+    */
+  @inline
+  def initializeFileStream(path: String,
+                           chunkSize: Int = defaultChunkSize): FileStream[Task, String] = {
+    io
+      .file
+      .readAll[Task](Paths.get(path), chunkSize)
+      .through(text.utf8Decode)
+  }
+}
---
+++ src/test/scala/gnormalizer/FileStreamInitializerTest.scala
@@ -0,0 +1,45 @@
+package gnormalizer
+
+import org.specs2.mutable.Specification
+
+/**
+  * Test for @see [[FileStreamInitializer]]
+  */
+class FileStreamInitializerTest extends Specification {
+
+  "initializeFileStream() method" should {
+    val oneElementEdgeFilePath = "src/test/resources/edge_list/test_edge_list_one_element"
+    s"Works with files with no break lines: '$oneElementEdgeFilePath'" in {
+      val rawFileContents: Vector[String] = {
+        FileStreamInitializer.
+          initializeFileStream(oneElementEdgeFilePath).
+          runLog.
+          unsafeRun()
+      }
+      s"Contains only a streamed value (No split is done yet)" in {
+        rawFileContents must haveSize(1)
+      }
+      val expectedEdge: String = "0 1"
+      s"Streams the expected file content: '$expectedEdge'" in {
+        rawFileContents must contain(expectedEdge)
+      }
+    }
+
+    val simpleEdgeFilePath = "src/test/resources/edge_list/test_simple_edge_list"
+    s"Works with files with break lines: '$simpleEdgeFilePath'" in {
+      val rawFileContents: Vector[String] = {
+        FileStreamInitializer.
+          initializeFileStream(simpleEdgeFilePath).
+          runLog.
+          unsafeRun()
+      }
+      s"Contains only a streamed value (No split is done yet)" in {
+        rawFileContents must haveSize(1)
+      }
+      val expectedFileContent: String = "0 1\n1 2\n1 3\n1 9"
+      s"Streams the expected file content: '0 1\\1 2\\n1 3\\n1 9'" in {
+        rawFileContents must contain(expectedFileContent)
+      }
+    }
+  }
+}
