69c336bd22a22b81cd525ac53c32dad68d3c31e8
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2016-09-30 08:52:10 +0200

    Add Scaladoc to 'gnormalizer.models.Vertex'

--- src/main/scala/gnormalizer/models/Vertex.scala
+++ src/main/scala/gnormalizer/models/Vertex.scala
@@ -1,7 +1,17 @@
 package gnormalizer.models
 
+/**
+  * Type aliases for representing the Vertices identifiers in the application.
+  */
 object Vertex {
 
+  /**
+    * Type alias represents a non-normalized [[Vertex]].
+    */
   type InputVertex = String
+
+  /**
+    * Type alias representing the [[InputVertex]] index within the module.
+    */
   type VertexMapping = Long
 }
