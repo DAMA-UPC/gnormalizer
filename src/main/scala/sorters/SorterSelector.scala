858439fe168ac578c189dff056a12d8f4841cdb9
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2016-10-25 10:19:51 +0200

    Implement a 'SorterSelector' for choosing automatically a Sorter

---
+++ src/main/scala/sorters/SorterSelector.scala
@@ -0,0 +1,34 @@
+package sorters
+
+/**
+  * Object containing methods using for choosing a [[Sorter]] between
+  * the [[DiskSorter]] and the [[InMemorySorter]].
+  */
+object SorterSelector {
+
+  @inline private[this] val bytesInKilobyte: Int = 1024
+  @inline private[this] val bytesInMegabyte: Long = bytesInKilobyte * 1024L
+  @inline private[this] val bytesInGigabyte: Long = bytesInMegabyte * 1024L
+
+  /**
+    * Threshold used used for making the decision between using
+    * the [[InMemorySorter]] or the [[DiskSorter]].
+    */
+  val thresholdToDiskSorterInKilobytes: Long = bytesInGigabyte // 1 Gigabyte
+
+  /**
+    * Obtains the [[Sorter]] needed to sort an input file. It can be
+    * either a [[DiskSorter]] or as [[InMemorySorter]], choosing the [[InMemorySorter]]
+    * if the whole graph fits entirely into memory.
+    *
+    * @param fileSizeInBytes from the file that will be sorted by the output [[Sorter]].
+    * @return the [[Sorter]] that will be used to sort the input graph.
+    */
+  def sorterFromFile(fileSizeInBytes : Long): Sorter = {
+    if (fileSizeInBytes < bytesInGigabyte) {
+      new InMemorySorter()
+    } else {
+      new DiskSorter()
+    }
+  }
+}
---
+++ src/test/scala/sorters/SorterSelectorTest.scala
@@ -0,0 +1,22 @@
+package sorters
+
+import org.specs2.mutable.Specification
+
+/**
+  * Test cases for @see [[SorterSelector]]
+  */
+class SorterSelectorTest extends Specification {
+
+  "When the file size is lower than the threshold, return a InMemorySorter " in {
+    val fileSizeLowerThreshold: Long = SorterSelector.thresholdToDiskSorterInKilobytes - 1
+    SorterSelector.sorterFromFile(fileSizeLowerThreshold) must beAnInstanceOf[InMemorySorter]
+  }
+  "When the file size is equal to the threshold, return a InMemorySorter " in {
+    val fileSizeEqualToThreshold: Long = SorterSelector.thresholdToDiskSorterInKilobytes
+    SorterSelector.sorterFromFile(fileSizeEqualToThreshold) must beAnInstanceOf[DiskSorter]
+  }
+  "When the file size is greater than the threshold, return a DiskSorter " in {
+    val fileSizeGreaterThreshold: Long = SorterSelector.thresholdToDiskSorterInKilobytes + 1
+    SorterSelector.sorterFromFile(fileSizeGreaterThreshold) must beAnInstanceOf[DiskSorter]
+  }
+}
