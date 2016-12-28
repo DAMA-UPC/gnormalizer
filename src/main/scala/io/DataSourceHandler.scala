a6ffc67f74727ad705e3fea8f8ba672f548d370c
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2016-12-28 12:03:09 +0100

    Add base trait for all DataSources handlers

---
+++ src/main/scala/io/DataSourceHandler.scala
@@ -0,0 +1,17 @@
+package io
+
+import fs2.{Task, Stream}
+
+/**
+  * Object containing a set of methods used for initializing file [[Stream]]s.
+  */
+trait DataSourceHandler {
+
+  /**
+    * Initializes a [[Stream]] on the given source.
+    *
+    * @param path where the [[Stream]] will be initialized.
+    * @return a [[Stream]] that will be return the graph contents as UTF-8 [[String]]'s.
+    */
+  def init(path: String): Stream[Task, String]
+}
