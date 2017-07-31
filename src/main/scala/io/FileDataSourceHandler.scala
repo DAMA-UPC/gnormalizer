94c6e96291f92e0e04e70b6875a53c09bf53dc2a
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2017-07-31 18:09:14 +0200

    Move 'chunkSize' to a companion object

--- src/main/scala/io/FileDataSourceHandler.scala
+++ src/main/scala/io/FileDataSourceHandler.scala
@@ -9,16 +9,22 @@ import fs2.{Task, io, text, Stream => FileStream}
   */
 class FileDataSourceHandler extends DataSourceHandler {
 
-  @inline val chunkSize: Int = 4096
-
   /**
     * @inheritdoc
     */
   @inline
   override def init(path: String): FileStream[Task, String] = {
     io.file
-      .readAll[Task](Paths.get(path), chunkSize)
+      .readAll[Task](Paths.get(path), FileDataSourceHandler.chunkSize)
       .through(text.utf8Decode)
       .through(text.lines)
   }
+}
+
+object FileDataSourceHandler {
+
+  /**
+    * Maximum size of the data chunks being
+    */
+  @inline val chunkSize: Int = 4096
 }
