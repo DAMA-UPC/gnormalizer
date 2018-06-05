2811963982a88de7f0b9d7cae4141b3b59c8e291
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2018-06-04 18:55:14 -0700

    Fix Codacy issues

--- src/test/scala/gnormalizer/solvers/GraphObfuscatorEndToEndSpec.scala
+++ src/test/scala/gnormalizer/solvers/GraphObfuscatorEndToEndSpec.scala
@@ -10,11 +10,18 @@ class GraphObfuscatorEndToEndSpec extends Specification {
 
   private[this] val testFile = "./src/test/resources/edge_list/test_edge_list_to_obfuscate"
 
+  private[this] val numberDistinctVertexInFile: Int = 5
+
+  private[this] val numberEdgesInFile: Int = 4
+
   "GraphObfuscator" should {
     "Obfuscate all input graph elements and put them on order" in {
       val result = GraphObfuscator.obfuscateEdgeListGraph(testFile).unsafeRunSync().toList
-      (0 to 4).map(v => result.exists(e => e.source == v || e.target == v) must beTrue)
-      result.size must beEqualTo(4)
+
+      (0 until numberDistinctVertexInFile)
+        .map(v => result.exists(e => e.source == v || e.target == v) must beTrue)
+
+      result.size must beEqualTo(numberEdgesInFile)
       result must beEqualTo(result.sorted)
     }
   }
