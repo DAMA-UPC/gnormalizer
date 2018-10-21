1d97a9cc9a5b4692befb37576b3adbbfe8a7dc1a
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2018-10-21 11:35:28 -0700

    Minor style clean-up on 'FileStreamInitializerSpec'

--- src/test/scala/gnormalizer/io/FileStreamInitializerSpec.scala
+++ src/test/scala/gnormalizer/io/FileStreamInitializerSpec.scala
@@ -7,13 +7,10 @@ import org.specs2.mutable.Specification
   */
 class FileStreamInitializerSpec extends Specification {
 
-  def initLoggedFileStream(filePath: String): Vector[String] =
-    new FileDataSourceHandler().init(filePath).compile.toList.unsafeRunSync().toVector
-
   "initializeFileStream() method" should {
     val oneElementEdgeFilePath = "src/test/resources/edge_list/test_edge_list_one_element"
     s"Works with files with a single element on it: '$oneElementEdgeFilePath'" in {
-      val rawFileContents: Vector[String] = initLoggedFileStream(oneElementEdgeFilePath)
+      val rawFileContents: Vector[String] = getPersistedFileContent(oneElementEdgeFilePath)
       val expectedEdge: Seq[String] = Vector("0 1")
       s"Streams the expected file content: '$expectedEdge'" in {
         rawFileContents must beEqualTo(expectedEdge)
@@ -22,11 +19,20 @@ class FileStreamInitializerSpec extends Specification {
 
     val simpleEdgeFilePath = "src/test/resources/edge_list/test_simple_edge_list"
     s"Works with files with break lines: '$simpleEdgeFilePath'" in {
-      val rawFileContents: Vector[String] = initLoggedFileStream(simpleEdgeFilePath)
+      val rawFileContents: Vector[String] = getPersistedFileContent(simpleEdgeFilePath)
       val expectedFileContent: Seq[String] = Vector("0 1", "1 2", "1 3", "1 9")
       s"Streams the expected file content: '${expectedFileContent.toString}'" in {
         rawFileContents must beEqualTo(expectedFileContent)
       }
     }
   }
+
+  private[this] def getPersistedFileContent(filePath: String): Vector[String] =
+    new FileDataSourceHandler()
+      .init(filePath)
+      .compile
+      .toList
+      .unsafeRunSync()
+      .toVector
+
 }
