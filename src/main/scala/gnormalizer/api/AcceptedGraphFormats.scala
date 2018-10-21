068ec1793727aa2426c472ec8d9295a952c046d0
Xavier Fernández Salas <xavier.fernandez.salas@gmail.com>
2018-10-21 10:41:56 -0700

    Minor Scaladoc improvements

--- src/main/scala/gnormalizer/api/AcceptedGraphFormats.scala
+++ src/main/scala/gnormalizer/api/AcceptedGraphFormats.scala
@@ -2,13 +2,13 @@ package gnormalizer.api
 import babel.graph.GraphFormat
 
 /**
- * Contains a list with the accepted graph types for the input and outputs.
- */
+  * Contains a list with the accepted graph formats for the library input and outputs.
+  */
 trait AcceptedGraphFormats {
   case object EdgeList extends GraphFormat
 }
 
 /**
- * @see [[AcceptedGraphFormats]]
- */
+  * @see [[AcceptedGraphFormats]]
+  */
 object AcceptedGraphFormats extends AcceptedGraphFormats
